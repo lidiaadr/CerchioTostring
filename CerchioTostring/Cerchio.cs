c47b14d3952e1998d1f666b9bddb51f6c10f1c61
acer <acer@LAPTOP-H9QES7A9>
2024-01-24 19:44:13 +0100

    Aggiungere i file di progetto.

---
+++ CerchioTostring/Cerchio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerchioTostring
+{
+    internal class Cerchio
+    {
+        
+
+        public float Raggio { get; set; }
+        
+
+        public Cerchio(float raggio)
+        {
+            Raggio = raggio;
+        }
+
+        public float Area()
+        {
+            return (float)(Math.PI*Raggio*Raggio);
+        }
+        public float Perimetro()
+        {
+            return (float)(Math.PI * (Raggio * 2));
+        }
+
+        public override string ToString()
+        {
+            return string.Format(" {0} ", Raggio);
+        }
+    }
+}
---
+++ CerchioTostring/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CerchioTostring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float r = float.Parse(Console.ReadLine());
+            Cerchio c = new Cerchio(r);
+            Console.WriteLine(c.Area());
+            Console.WriteLine(c.Perimetro());
+            Console.WriteLine(c.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
